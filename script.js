3d0975ef9fd1e69d97a8399bcfa22953a5f06db1
LucasPetros <lucas.petros@outlook.com>
2021-08-14 21:42:20 -0300

    Snake Game HTML/CSS/JS ATT TITULO E BG

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "yellow";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for (i=0; i < snake.length; i++) {
-        context.fillStyle = "blue";
+        context.fillStyle = "#964b00";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "#993399";
     context.fillRect(food.x, food.y, box, box);
 }
 
